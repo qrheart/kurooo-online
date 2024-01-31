1a082e4a17f08b80b25e13d80ab6ab0437761e77
qrheart <evgeny.karhanov@yandex.ru>
2024-02-01 01:18:43 +0300

    add hydra & hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1 class="text-red-300 font-extrabold">Welcome World</h1>
-<p>(ﾉ≧∀≦)ﾉ・‥…炎炎炎炎炎炎炎炎</p>
+<script>
+	// @ts-nocheck
+
+	const DWIDTH = 1536;
+	const DHEIGHT = 740;
+
+	import { onMount, tick } from 'svelte';
+
+	let canvas;
+
+	onMount(async () => {
+		const hydraModule = await import('hydra-synth');
+		const Hydra = hydraModule.default;
+
+		const h = new Hydra({
+			canvas: canvas,
+			makeGlobal: false,
+			detectAudio: false
+		}).synth;
+
+		h.osc(24, 0, 0.037)
+			.kaleid()
+			.mask(h.shape(5, 0.03, 1))
+			.modulateRotate(h.shape(1.782, 0.15, 0.032))
+			.modulateRotate(h.shape(2, 0.01, 5.304))
+			.modulateRotate(h.shape(3, 0.001, 2.045))
+			.scale(0.6)
+			.mult(h.shape(4, 0.2, 1).color(0.1, 0.01, 0.1, 0.5))
+			.rotate(() => h.time * 0.039)
+			.out();
+		h.render();
+	});
+</script>
+
+<div class="w-screen h-screen grid place-content-center bg-black">
+	<canvas class="fixed" bind:this={canvas} width={DWIDTH * 2} height={DHEIGHT * 2} />
+	<h1 class="font-extrabold text-3xl mix-blend-lighten text-red-900">
+		hey, eugene here! <br />
+		wanna see some cool wwweb magic? <br />
+		(ﾉ≧∀≦)ﾉ・‥…炎炎炎炎炎炎炎炎
+	</h1>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'display': 'Inconsolata, Arial, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
